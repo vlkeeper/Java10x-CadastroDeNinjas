1aeec87e996ed31b5af920ee49b3d6f99ce2b3bc
Vinicius Leandro Oliveira de Souza <viniciusleandroods@hotmail.com>
2025-11-05 15:16:02 -0400

    Criado a Controller / Aula 02

---
+++ src/main/java/aluno/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package aluno.dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Controlador para APIs REST
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas") //Anotação que pega informações e mapeia
+    public String boasVindas() {
+        return "Esta é a minha primeira mensagem nessa rota!";
+    }
+}
