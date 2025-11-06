5d4b7e057de88f78f3d8b9781fe2431fd9a194be
Vinicius Leandro Oliveira de Souza <viniciusleandroods@hotmail.com>
2025-11-06 13:06:21 -0400

    feat: Utilizando o Lombok nas classes para abstração

--- src/main/java/aluno/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/aluno/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package aluno.dev.java10x.CadastroDeNinjas.Missoes;
 
 import aluno.dev.java10x.CadastroDeNinjas.Ninja.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -18,27 +24,4 @@ public class MissoesModel {
     @OneToMany //Uma missão pode ter vários ninjas
     private List<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String missao, char rank) {
-        this.missao = missao;
-        this.rank = rank;
-    }
-
-    public String getMissao() {
-        return missao;
-    }
-
-    public void setMissao(String missao) {
-        this.missao = missao;
-    }
-
-    public char getRank() {
-        return rank;
-    }
-
-    public void setRank(char rank) {
-        this.rank = rank;
-    }
 }
--- src/main/java/aluno/dev/java10x/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/aluno/dev/java10x/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -2,10 +2,16 @@ package aluno.dev.java10x.CadastroDeNinjas.Ninja;
 
 import aluno.dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //JPA = Java Persistent API
 @Entity //Transforma a classe em uma entidade do banco de dados
 @Table(name = "tb_cadastro") //Dá um nome à tabela / Uma boa prática é o nome ser minúsculo e com o underline (snake case)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaModel {
 
     @Id //Informa que a tabela trabalha com ID
@@ -18,36 +24,4 @@ public class NinjaModel {
     @JoinColumn (name = "missoes_id") //Criando a foreign key (chave estrangeira)
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
